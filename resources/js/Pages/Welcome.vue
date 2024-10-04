9987b8bcb20065cde0c78d7aa46109b520c836f3
Esteban Sahid <esteban_ct28@hotmail.com>
2024-10-04 13:41:46 +0200

    Login and First Commit El Hiper

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+function handleImageError() {
+    document.getElementById('screenshot-container')?.classList.add('!hidden');
+    document.getElementById('docs-card')?.classList.add('!row-span-1');
+    document.getElementById('docs-card-content')?.classList.add('!flex-row');
+    document.getElementById('background')?.classList.add('!hidden');
+}
+</script>
+
+<template>
+    <Head title="Login" />
+    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+        <!-- Estilo y imagen a gusto 
+        <img
+            id="background"
+        />
+        -->
+        <div
+            class="relative flex min-h-screen flex-col justify-center selection:bg-[#FF2D20] selection:text-white"
+        >
+            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                <header
+                >
+                    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+                        <Link
+                            v-if="$page.props.auth.user"
+                            :href="route('dashboard')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Dashboard
+                        </Link>
+
+                        <template v-else>
+                            <div class="-mx-3 flex flex-1 justify-end">
+
+                                <Link
+                                    :href="route('login')"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Log in
+                                </Link>
+    
+                                <Link
+                                    v-if="canRegister"
+                                    :href="route('register')"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Registro
+                                </Link>
+                            </div>
+                        </template>
+                    </nav>
+                </header>
+
+                <footer
+                    class="py-16 text-center text-sm text-black dark:text-white/70"
+                >
+                    Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
