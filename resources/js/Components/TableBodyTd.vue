8a21faeb0655f0b13198dd3f65c8f1aef6840c02
Esteban Sahid <esteban_ct28@hotmail.com>
2024-11-22 16:31:12 +0100

    add z-index a la tabla para sobreposicion

--- resources/js/Components/TableBodyTd.vue
+++ resources/js/Components/TableBodyTd.vue
@@ -1,6 +1,6 @@
 <template>
     <td
-        class="border-t dark:border-gray-900 px-6 py-4 text-sm text-gray-900 dark:text-gray-200"
+        class="border-t dark:border-gray-900 px-6 py-4 text-sm text-gray-900 dark:text-gray-200 z-0"
     >
         <slot />
     </td>   
--- resources/js/Components/TableBodyTr.vue
+++ resources/js/Components/TableBodyTr.vue
@@ -1,7 +1,7 @@
 <template>
     <tr
         class="hover:bg-gray-100 dark:focus-within:bg-gray-800 focus-within:bg-gray-100 text-center
-        dark:hover:bg-gray-900"
+        dark:hover:bg-gray-900 z-0"
     >
         <slot />
     </tr>   
