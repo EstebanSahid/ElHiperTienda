8d86fa3c53d92fceeaac7e92cc930f64e06dcf40
Esteban Sahid <esteban_ct28@hotmail.com>
2024-11-22 16:31:50 +0100

    scroll

--- resources/js/Pages/Orders/InsertOrder.vue
+++ resources/js/Pages/Orders/InsertOrder.vue
@@ -28,6 +28,21 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
             </div>
         </template>
 
+        <!-- Scroll -->
+        <div v-if="enableScrollButton" class="fixed bottom-6 left-1/2 z-10 transition-transform duration-200 ease-in-out hover:scale-110">
+            <button 
+                type="button" 
+                class="group flex items-center justify-center border-2 w-12 h-12 rounded-full hover:shadow-lg
+                bg-white border-gray-400 hover:border-gray-400
+                dark:bg-gray-700 dark:border-gray-900 dark:hover:border-gray-900"
+                @click="scrollTop"
+            >
+                <svg class="block w-6 h-6 fill-gray-400 group-hover:fill-gray-400 dark:fill-gray-900 dark:group-hover:fill-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M4 12l1.41 1.41L11 7.83v12.34h2V7.83l5.59 5.58L20 12l-8-8z" />
+                </svg>
+            </button>
+        </div>
+
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
@@ -181,10 +196,29 @@ export default {
             }),
 
             productosOrden: [],
+
+            enableScrollButton: false,
             fechaActual: new Date().toISOString().slice(0, 10)
         }
     },
     methods: {
+        // Monitorear scroll
+        handleScroll() {
+            console.log("Scroll hecho")
+            console.log(window.scrollY);
+            this.enableScrollButton = window.scrollY > 400
+        },
+
+        // Regresar scroll
+        scrollTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+
+            this.enableScrollButton = false;
+        },
+
         // Quitar un producto de la orden
         deleteProductArray(index){
             this.productosOrden.splice(index, 1);
@@ -290,12 +324,11 @@ export default {
     },
     
     mounted() {
-        /*
-        console.log(this.productos)
-        console.log("id tienda")
-        console.log(this.tienda);
-        */
-    }
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
 }
 </script>
 
