cd1c10a19d37365f32848dc0ee13f6b232668e10
Esteban Sahid <esteban_ct28@hotmail.com>
2024-12-15 15:30:55 +0100

    Refactorizacion helper hora

--- resources/js/Services/DateHelper.js
+++ resources/js/Services/DateHelper.js
@@ -1,15 +1,22 @@
+/* 
+Se captura la zona horaria presente en el archivo .env y
+damos las opciones de fecha
+*/
 const timeZone = process.env.APP_TIMEZONE;
+const opciones = { timeZone, year: 'numeric', month: '2-digit', day: '2-digit' };
 
-export const obtenerFechaActual = () => {
+const formatearFecha = (formatoSalida) => {
     const fechaActual = new Date();
-    const opciones = { timeZone, year: 'numeric', month: '2-digit', day: '2-digit' };
     const [dia, mes, anio] = new Intl.DateTimeFormat('es-ES', opciones).format(fechaActual).split('/');
-    return `${dia}/${mes}/${anio}`
-}
+    
+    if (formatoSalida === 'dd/mm/yyyy') {
+        return `${dia}/${mes}/${anio}`;
+    } else if (formatoSalida === 'yyyy-mm-dd') {
+        return `${anio}-${mes}-${dia}`;
+    }
+    throw new Error(`Formato de salida no soportado: ${formatoSalida}`);
+};
 
-export const obtenerFechaActualGuardarBD = () => {
-    const fechaActual = new Date();
-    const opciones = { timeZone, year: 'numeric', month: '2-digit', day: '2-digit' };
-    const [dia, mes, anio] = new Intl.DateTimeFormat('es-ES', opciones).format(fechaActual).split('/');
-    return `${anio}-${mes}-${dia}`
-}
+export const obtenerFechaActual = () => formatearFecha('dd/mm/yyyy');
+
+export const obtenerFechaActualGuardarBD = () => formatearFecha('yyyy-mm-dd');
