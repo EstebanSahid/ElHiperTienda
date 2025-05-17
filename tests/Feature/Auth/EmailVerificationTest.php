b5360e854a261a01f0fe3fbcdab6223b44c0822e
Esteban Sahid <esteban_ct28@hotmail.com>
2025-05-17 16:36:37 +0200

    Add rol factory a user factory

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
+use App\Models\Rol;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
